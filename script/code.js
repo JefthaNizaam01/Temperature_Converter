f3b13c78d41a987d701f9982568d0cb4edf88644
Name <nizaam01.jeftha@gmail.com>
2024-05-17 09:46:57 +0200

    Initial_commit

---
+++ script/code.js
@@ -0,0 +1,13 @@
+function convertTemperature() {
+    let temperature = parseFloat(document.querySelector('input[type="number"]').value);
+    let conversionType = document.getElementById('Conversion').value;
+    let result; 
+    if (conversionType === "Celsuis to Fahrenheit") {
+        result = (temperature * 9/5) + 32;
+    } else {
+        result = (temperature - 32) * 5/9;
+    }
+
+    let resultElement = document.getElementById('result');
+resultElement.textContent = Math.round(result * 100) / 100;
+}
